0124e5b37ad8aae84250202f1574ff0c873c41c2
raviNavarro1226 <ravi.navarro@madeinweb.app>
2022-10-15 17:48:29 -0300

    first commit

---
+++ TCA-Counter/TCA-Counter/TCA_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_CounterApp.swift
+//  TCA-Counter
+//
+//  Created by Ravi navarro on 15/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TCA_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
